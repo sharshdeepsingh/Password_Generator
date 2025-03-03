fc2ebf165a34a6233ae415421b14230a5651220e
Harshdeep Singh Saini <harshdeepsinghsaini2023@gmail.com>
2025-03-03 16:40:12 +0530

    debugging the numbers only and characters only error undefined

--- src/hooks/usePasswordGenerator.js
+++ src/hooks/usePasswordGenerator.js
@@ -1,47 +1,50 @@
 import { useState } from "react";
 
-const usePasswordGenerator=()=>{
-const[password,setPassword]=useState();
-const[errorMessage,setErrorMessage]=useState();
+const usePasswordGenerator = () => {
+  const [password, setPassword] = useState("");
+  const [errorMessage, setErrorMessage] = useState("");
 
-const generatePassword=(checkBoxData,length)=>{
-    let charset="";
-    let generatedPassword="";
+  const generatePassword = (checkBoxData, length) => {
+    let charset = "";
+    let generatedPassword = "";
 
-    const selectedOption=checkBoxData.filter((checkbox)=>checkbox.state)
-    if(selectedOption.length===0)
-    {
-        setErrorMessage("Select at least one option");
-        setPassword("");
-        return;
+    // Filter selected options
+    const selectedOptions = checkBoxData.filter((checkbox) => checkbox.state);
+
+    if (selectedOptions.length === 0) {
+      setErrorMessage("Select at least one option");
+      setPassword("");
+      return;
     }
 
-    selectedOption.forEach((option)=> {
-        switch(option.title){
-            case"Include Alphabets":
-            charset+="ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz";
-            break;
-            case"Include Number":
-            charset+="0123456789";
-            break;
-            case"Include Charaters":
-            charset+="~!@#$%^&*()";
-            break; 
-        }
+    
+    selectedOptions.forEach((option) => {
+      switch (option.title) {
+        case "Include Alphabets":
+          charset += "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz";
+          break;
+        case "Include Numbers": 
+          charset += "0123456789";
+          break;
+        case "Include Characters":
+          charset += "~!@#$%^&*()";
+          break;
+        default:
+          break;
+      }
     });
 
-    for(let i=0;i<length;i++)
-    {
-        const randomIndex=Math.floor(Math.random()*charset.length);
-        generatedPassword+=charset[randomIndex];
+    // Generate Password
+    for (let i = 0; i < length; i++) {
+      const randomIndex = Math.floor(Math.random() * charset.length);
+      generatedPassword += charset[randomIndex];
     }
 
     setPassword(generatedPassword);
     setErrorMessage("");
-}
+  };
 
-return {password,errorMessage,generatePassword};
-
-}
+  return { password, errorMessage, generatePassword };
+};
 
 export default usePasswordGenerator;
